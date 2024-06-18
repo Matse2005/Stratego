22adbadd96e3fe3f1631480701d712810102c2cf
MatseVH <49431416+Matse2005@users.noreply.github.com>
2024-06-18 18:13:08 +0200

    git added meta image

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,12 +9,21 @@ export const metadata = {
     "Maak aan de hand van deze tool snel en gemakkelijk volledig aanpasbare stratego kaartjes!",
   keywords:
     "Stratego, Generator, Online, Stratego Kaartjes, Kaartjes, Levende Startego, Jeugdwerk",
+  "og:image": "https://stratego.jeugdwerk.org/meta.png",
+  "twitter:image": "https://stratego.jeugdwerk.org/meta.png",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className + "  bg-gray-100"}>{children}</body>
+      <body className={inter.className + "  bg-gray-100"}>
+        {children}
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="745f3276-ffbe-4010-8636-6c437de52bf5"
+        ></script>
+      </body>
     </html>
   );
 }
