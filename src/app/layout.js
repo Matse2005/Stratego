4c1378981a0c130208daf16909a957c1554ddf78
MatseVH <49431416+Matse2005@users.noreply.github.com>
2024-06-23 18:57:02 +0200

    Edit metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Stratego Kaartjes",
+  title: "Stratego Kaartjes | Jeugdwerk",
   description:
     "Maak aan de hand van deze tool snel en gemakkelijk volledig aanpasbare stratego kaartjes!",
   keywords:
